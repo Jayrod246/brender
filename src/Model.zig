2cf33766cdb01e90079adbfc50c09b2231e640c5
Jarrod Meyer <meyerja.2013@gmail.com>
2024-02-27 18:01:12 -0500

    Model: make getBuffers() return null for empty mesh

--- src/Model.zig
+++ src/Model.zig
@@ -10,8 +10,12 @@ pub const alloc = BrModelAllocate;
 pub const load = BrModelLoad;
 pub const add = BrModelAdd;
 
-pub inline fn getBuffers(self: *Model) struct { vertices: []br.Vertex, faces: []br.Face } {
-    return .{ .vertices = self.m.vertices[0..self.m.nvertices], .faces = self.m.faces[0..self.m.nfaces] };
+pub inline fn getBuffers(self: *Model) ?struct { vertices: []br.Vertex, faces: []br.Face } {
+    if (self.m.nvertices == 0) return null;
+    return .{
+        .vertices = self.m.vertices[0..self.m.nvertices],
+        .faces = self.m.faces[0..self.m.nfaces],
+    };
 }
 
 extern fn BrModelAllocate(name: [*c]const u8, num_vertices: c_int, num_faces: c_int) ?*Model;
